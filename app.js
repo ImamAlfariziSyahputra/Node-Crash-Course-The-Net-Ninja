ed964462092e159b0f9f6b4d3fef58dccbe2baee
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-05-08 14:51:08 +0700

    partials

--- app.js
+++ app.js
@@ -10,20 +10,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  // res.send('<p>Home Page</p>');
-  res.render('index');
+  const blogs = [
+    {title: 'Lazy finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
+    {title: 'Gero finds stars', snippet: 'lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat thanos', snippet: 'lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>About Page</p>');
-  res.render('about');
+  res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', { title: 'Create a New Blog' });
 })
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { title: '404 Not Found' });
 })
