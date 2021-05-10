e1989df45a8aa15cc503e1fe38d047b93621e32c
ImamAlfariziSyahputra <imam.alfarizi.777@gmail.com>
2021-05-10 10:28:44 +0700

    try MongoDB Mongoose

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// connect to MongoDB
+const dbURI = 'mongodb+srv://mamlzy:mamlzy1421@nodetuts.iivtu.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
 // View Engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('tiny'));
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Lazy finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
-    {title: 'Gero finds stars', snippet: 'lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat thanos', snippet: 'lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {
